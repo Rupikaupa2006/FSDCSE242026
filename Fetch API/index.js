1c8cdc37cfe76d686c6d471088adb51e1fefdc87
Rupikaupa2006 <rupikaupadhyay3@gmail.com>
2026-08-25 10:07:20 +0530

    added fetchapi file

---
+++ Fetch API/index.js
@@ -0,0 +1,6 @@
+const root=document.getElementById('container');
+const button=document.getElementById('btn');
+function fetchData(){
+    alert("Hiiii");
+}
+button.addEventListener('click',fetchData);
