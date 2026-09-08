fc351c62dea3b6038823fd866fe49b9ca4016c8e
Rupikaupa2006 <rupikaupadhyay3@gmail.com>
2026-09-08 10:28:20 +0530

    ReactVite

---
+++ reactdom/index.js
@@ -0,0 +1,37 @@
+const container = document.getElementById('root');
+
+console.log(container);
+
+const root = ReactDOM.createRoot(container);
+
+// here we are creating a root element for ReactDOM
+
+const h2 = React.createElement(
+    'h2',
+    { style: { color: 'red', backgroundColor: 'cyan' } },
+    'Welcome to ReactDOM'
+);
+
+// here we are creating an element
+
+const h1 = React.createElement(
+    'h1',
+    { style: { color: 'brown' } },
+    'ABES Engineering College'
+);
+const img= React.createElement('img',{src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4W5se-3sXcI-CuvSm5GbPoSk655stnvqEeWyX1M79KA&s=10',style:{height:'200px',width:'200px'}});
+
+// in React.createElement we can provide style simultaneously
+
+const div = React.createElement(
+    'div',
+    { style: { border: '2px solid red' } },
+    img,
+    h1,
+    h2
+);
+
+
+const h21=<h2>Hello World</h2>//here we are creating an element using JSX(javascript XML)
+//for converting jsx to js we use babel compiler which is already included in react library. we are converting it to js because browser can only understand js not jsx
+root.render(h21);
